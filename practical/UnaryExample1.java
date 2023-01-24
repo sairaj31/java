ac79bec970e66b520aeb281a9240da18a91f9374
sairaj31 <123162579+sairaj31@users.noreply.github.com>
2023-01-24 20:36:00 +0530

    Add files via upload

---
+++ practical/UnaryExample1.java
@@ -0,0 +1,27 @@
+class UnaryExample1{
+public void calcIncrement(){
+	int x=10;
+	System.out.println(x++);
+	System.out.println(x++);
+	System.out.println(++x);
+	System.out.println(x++);
+	System.out.println(x);
+}
+
+public void calcDecrement(){
+	int x=10;
+	 
+		System.out.println(x--);
+		System.out.println(--x);
+		System.out.println(--x);
+		System.out.println(x--);
+		System.out.println(x);
+}
+
+public static void main(String args[]){
+	UnaryExample obj=new UnaryExample();
+	 obj.calcIncrement();
+	 obj.calcDecrement();
+	
+}
+}
