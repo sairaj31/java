ca291b37e111886037b84b6ec616e7f64661d6d8
sairaj31 <123162579+sairaj31@users.noreply.github.com>
2023-02-02 22:04:09 +0530

    Add files via upload

---
+++ lab/numbers1.java
@@ -0,0 +1,12 @@
+ class numbers1{
+ public static void main(String args[]){
+	System.out.println("\n;Divisible by 5:");
+	for(int i=40;i<=100;i=i+5){
+	if (i%5==0)  //condition
+		System.out.println(i +", ");  //print number
+	
+	}
+	System.out.println("/n");
+}
+
+}
