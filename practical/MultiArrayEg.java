89b700e34b584fb13b3096e5fff02b1aac1938ed
sairaj31 <123162579+sairaj31@users.noreply.github.com>
2023-02-17 22:02:36 +0530

    Add files via upload

---
+++ practical/MultiArrayEg.java
@@ -0,0 +1,19 @@
+
+
+ class MultiArrayEg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		int arr1[][] = {{9,8,7},{6,5,4},{3,2,1}};  //declaring & initialization
+		
+			//print
+			for(int i=0;i<3;i++) {
+				for(int j=0;j<3;j++)
+					System.out.print(arr1[i][j] + "  " );
+			}
+				System.out.println();
+
+	}
+
+}
