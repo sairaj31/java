25b26c887517910722ffe33629bc24b0f54fca80
sairaj31 <123162579+sairaj31@users.noreply.github.com>
2023-02-23 22:06:38 +0530

    Add files via upload

---
+++ lab/Shape.java
@@ -0,0 +1,8 @@
+package geometry;
+
+public class Shape {
+	public static void main(String[] args) { // Defines the main method for the class.
+//		Circle circle = new Circle(); // Creates a new Circle object with a default radius of 1.5.
+
+	}
+}
