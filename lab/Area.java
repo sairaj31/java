aad6496ef99a018f9b366f174a784a5927d1c8f8
sairaj31 <123162579+sairaj31@users.noreply.github.com>
2023-02-23 22:06:13 +0530

    Add files via upload

---
+++ lab/Area.java
@@ -0,0 +1,8 @@
+package Shapes;
+
+public class Area {
+	public static void main(String[] args) { // Defines the main method for the class.
+		Circle circle = new Circle(25); // Creates a new Circle object with a radius of 25.
+	}
+
+}
