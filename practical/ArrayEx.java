c7b6bf244572665466693129a94d294abe3a7ad8
sairaj31 <123162579+sairaj31@users.noreply.github.com>
2023-03-13 21:51:40 +0530

    Add files via upload

---
+++ practical/ArrayEx.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrayEx {
+		    public static void main(String [] args) {
+		        ArrayList<String> color = new ArrayList<String>();
+		        color.add("Green");
+		        color.add("Yellow");
+		        color.add("Black");
+		        color.add("Red");
+		        System.out.print(color);
+		   }
+
+}
